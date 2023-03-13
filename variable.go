012369483400fe76b8703d6f7b0ef4e9d242a79f
Aldi Resaldi Maulana <aldi.resaldi.intern@socialbread.id>
2023-03-13 10:39:15 +0700

    variable aja si

---
+++ variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	Nama := "Aldi Resaldi Maulana"
+	var Age = 23
+	var Address = "Jl raya kebayoran lama no.7"
+	Nama = "Aldi Taher"
+
+	fmt.Println(Nama, Age, Address)
+}
