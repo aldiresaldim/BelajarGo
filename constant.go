ec5bc50c3c62e21810b86c2e9c9f419da5a2b5b9
Aldi Resaldi Maulana <aldi.resaldi.intern@socialbread.id>
2023-03-13 10:59:23 +0700

    godeblak

---
+++ constant.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const FirstName = "Aldi"
+	const MiddleName = "Resaldi"
+	const LastName = "Maulana"
+
+	fmt.Println(FirstName, MiddleName, LastName)
+}
