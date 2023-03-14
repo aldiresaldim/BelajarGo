0c537a9ffa7c4cc53336df156e2dafca72af4cf6
Aldi Resaldi Maulana <aldi.resaldi.intern@socialbread.id>
2023-03-14 12:31:06 +0700

    belaajr slice

---
+++ slice.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var months = [...]string{
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember",
+	}
+	var slice1 = months[2:4]
+	fmt.Println(slice1)
+	fmt.Println(len(slice1))
+	fmt.Println(cap(slice1))
+}
