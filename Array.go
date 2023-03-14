4f0e9f0ff67209f0e1b57b1f404180a294bb1a75
Aldi Resaldi Maulana <aldi.resaldi.intern@socialbread.id>
2023-03-14 11:18:45 +0700

    test

---
+++ Array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+	names[0] = "Aldi"
+	names[1] = "Resaldi"
+	names[2] = "Maulana"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+}
