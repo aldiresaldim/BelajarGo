639c7122bb0b34f2196442f658db322f2ded82ce
Aldi Resaldi Maulana <aldi.resaldi.intern@socialbread.id>
2023-03-20 10:52:43 +0700

    -

---
+++ function-anonymous.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Blacklist func(string) bool
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("You are Blockeed", name)
+	} else {
+		fmt.Println("Welcome", name)
+	}
+}
+
+func main() {
+	blacklist := func(name string) bool {
+		return name == "admin"
+	}
+
+	registerUser("admin", blacklist)
+	registerUser("Aldi", blacklist)
+}
