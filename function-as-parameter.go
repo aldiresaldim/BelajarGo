11107694f9e6dcd0cbc87054e76d5f262e466dac
Aldi Resaldi Maulana <aldi.resaldi.intern@socialbread.id>
2023-03-20 10:12:52 +0700

    lanjutan function value

---
+++ function-as-parameter.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func sayHelloWithFilter(name string, filter func(string) string) {
+	nameFiltered := filter(name)
+	fmt.Println("Hello", nameFiltered)
+}
+
+func spamFilter(name string) string {
+	if name == "Anjing" {
+		return "..."
+	} else {
+		return name
+	}
+}
+
+func main() {
+	sayHelloWithFilter("Aldi", spamFilter)
+	sayHelloWithFilter("Anjing", spamFilter)
+}
