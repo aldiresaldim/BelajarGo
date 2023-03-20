951271114c7318cadbf53d7a39b177ec44b34032
Aldi Resaldi Maulana <aldi.resaldi.intern@socialbread.id>
2023-03-20 14:30:06 +0700

    berintraksi data disekitarnya

---
+++ closure.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	counter := 0
+	increment := func() {
+		fmt.Println("Increment")
+		counter++
+	}
+
+	increment()
+	increment()
+	fmt.Println(counter)
+}
