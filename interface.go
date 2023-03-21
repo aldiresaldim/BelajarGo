8dfdc11cdeebc905bef40616e3b318f4bbbec9ba
Aldi Resaldi Maulana <aldi.resaldi.intern@socialbread.id>
2023-03-21 11:37:21 +0700

    -

---
+++ interface.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(hasName HasName) {
+	fmt.Println("Hello", hasName.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+func main() {
+	var Aldi Person
+	SayHello(Aldi)
+}
